680251bb326ca90811346efe2ed818dc50d50c11
Adit Mehta <71823729+AditM99@users.noreply.github.com>
2025-02-25 22:23:44 -0800

    Update fib_lazy.py

--- fib_lazy.py
+++ fib_lazy.py
@@ -1,12 +1,11 @@
-# explanations for member functions are provided in requirements.py
-from __future__ import annotations
-
+from __future__ import  annotations
 class FibNode:
     def __init__(self, val: int):
         self.val = val
         self.parent = None
         self.children = []
         self.flag = False
+        self.is_vacant = False
 
     def get_value_in_node(self):
         return self.val
@@ -20,12 +19,11 @@ class FibNode:
     def __eq__(self, other: FibNode):
         return self.val == other.val
 
-class FibHeap:
+class FibHeapLazy:
     def __init__(self):
         # you may define any additional member variables you need
         self.roots = []
         self.min = None
-        self.deleted = False
 
     def get_roots(self) -> list:
         return self.roots
@@ -36,37 +34,41 @@ class FibHeap:
         if self.min is None or node.val < self.min.val:
             self.min = node
         return node
-        
-    def delete_min(self) -> None:
-        if not self.min:
-            return
-        self.deleted = True
-        if self.min in self.roots:
-            self.roots.remove(self.min)
-        if not self.roots:
-            self.min = None
-        else:
-            self.merge()
 
-    def find_min(self) -> FibNode:
-        if not self.roots:
-            if self.min and self.deleted:
-                for child in self.min.children:
-                    child.parent = None
-                    self.roots.append(child)
-                self.merge()
-                self.min = self.find_min()
-                self.deleted = False 
+    def delete_min_lazy(self) -> None:
+        if self.min is None:
+            return
+
+        if self.min.is_vacant:
+            self.merge()
+            self.min.is_vacant = True
+
+        else:
+            self.min.is_vacant = True
+            # self.find_min_lazy()
+
+
+
+    def find_min_lazy(self) -> FibNode:
+        if self.min is None or self.min.is_vacant == True:
+            self.merge()
+            return self.min
         else:
             return self.min
 
     def decrease_priority(self, node: FibNode, new_val: int) -> None:
         node.val = new_val
-        
+
         if node.parent and node.val < node.parent.val:
             self.cut(node)
-        
-        if node.val < self.min.val:
+            parent = node.parent
+            while parent and parent.flag:
+                self.cut(parent)
+                parent = parent.parent
+            if parent:
+                parent.flag = True
+
+        if self.min is None or node.val < self.min.val:
             self.min = node
 
     def cut(self, node: FibNode):
@@ -79,11 +81,19 @@ class FibHeap:
 
     def merge(self):
         degree_table = {}
-        
+        new_roots = []
+
         while self.roots:
             node = self.roots.pop()
+            if node.is_vacant:
+                for child in node.children:
+                    child.parent = None
+                    new_roots.append(child)
+            else:
+                new_roots.append(node)
+        while new_roots:
+            node = new_roots.pop()
             degree = len(node.children)
-            
             while degree in degree_table:
                 other = degree_table.pop(degree)
                 if node.val > other.val:
@@ -91,11 +101,14 @@ class FibHeap:
                 node.children.append(other)
                 other.parent = node
                 degree += 1
-            
+
             degree_table[degree] = node
-        
+
         self.roots = list(degree_table.values())
-        self.min = min(self.roots, key=lambda node: node.val)
+        self.min = None
+        for node in self.roots:
+            if self.min is None or node.val < self.min.val:
+                self.min = node
 
     # feel free to define new methods in addition to the above
     # fill in the definitions of each required member function (above),
